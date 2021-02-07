35ddac8270b96fbf684c697034ab046c4fc4a356
Alexander Nikolaev <nikolaev.warrior@gmail.com>
2021-02-07 22:10:51 +0500

    Addition Tab Bar, 4 VC and 1 model

---
+++ LoginApp/Models/ModelForEverything.swift
@@ -0,0 +1,18 @@
+//
+//  ModelForEverything.swift
+//  LoginApp
+//
+//  Created by Александр on 07.02.2021.
+//
+
+struct User {
+    let username = "User"
+    let password = "Password"
+    let person: Person
+}
+
+struct Person {
+    let name = "Александр"
+    let surname = "Николаев"
+    let patronymic = "Александрович"
+}
